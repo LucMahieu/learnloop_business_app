a36dcc4f57e3809b8b1e6c733a2d5ab2b9d535ae
Luc Mahieu <lucmauricemahieu@gmail.com>
2024-06-12 10:45:01 +0200

    improved infrastructure and prompt

--- src/pages/1_Probleemstelling bepalen.py
+++ src/pages/1_Probleemstelling bepalen.py
@@ -1,89 +1,91 @@
 import streamlit as st
 from utils.openai_client import connect_to_openai
 
-# Configure the Streamlit page settings
 st.set_page_config(page_title="Beeckestijn", page_icon="🔵", layout='centered', initial_sidebar_state='auto')
 
-def initialize_session_state():
-    """
-    Initialize session variables if they don't exist.
-    """
-    if "openai_model" not in st.session_state:
-        st.session_state.openai_model = "gpt-4o"
+class Probleemstelling:
+    def __init__(self):
+        self.company_name = 'LearnLoop'
+        self.course = 'Customer Experience & Journey Management'
+        self.client = connect_to_openai()
+        self.initialize_session_state()
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    def initialize_session_state(self):
+        """
+        Initialize session variables if they don't exist.
+        """
+        if "openai_model" not in st.session_state:
+            st.session_state.openai_model = "gpt-4o"
 
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
 
-def display_chat_messages():
-    """
-    Display chat messages in the Streamlit app.
-    """
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"], avatar='🔵' if message["role"] == "assistant" else '🔘'):
-            st.markdown(message["content"])
+    def display_chat_messages(self):
+        """
+        Display chat messages in the Streamlit app.
+        """
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"], avatar='🔵' if message["role"] == "assistant" else '🔘'):
+                st.markdown(message["content"])
 
+    def add_to_assistant_responses(self, response):
+        """
+        Add the response message to the chat.
+        """
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
-def add_to_assistant_responses(response):
-    """
-    Add the response message to the chat.
-    """
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    def add_to_user_responses(self, user_input):
+        """
+        Add the user input to the chat.
+        """
+        st.session_state.messages.append({"role": "user", "content": user_input})
 
+    def generate_assistant_response(self, user_input):
+        """
+        Generate a response from the assistant.
+        """
+        role_prompt = open("./assets/prompts/probleemstelling_bepalen.txt").read() + f"\nBedrijf van cursist: {self.company_name}" + f"\nCursus: {self.course}"
+        stream = self.client.chat.completions.create(
+            model=st.session_state.openai_model,
+            messages=[
+                {"role": "system", "content": role_prompt},
+                *({"role": m["role"], "content": m["content"]} for m in st.session_state.messages)
+            ],
+            stream=True,
+        )
+        
+        return stream
 
-def add_to_user_responses(user_input):
-    """
-    Add the user input to the chat.
-    """
-    st.session_state.messages.append({"role": "user", "content": user_input})
+    def handle_user_input(self):
+        """
+        Process user input and generate a response.
+        """
+        if user_input := st.chat_input("Jouw antwoord"):
+            self.add_to_user_responses(user_input)
+            with st.chat_message("user", avatar='🔘'):
+                st.markdown(f"{user_input}")
 
+            with st.chat_message("assistant", avatar='🔵'):
+                response = st.write_stream(self.generate_assistant_response(user_input))
+                self.add_to_assistant_responses(response)
+            
+            if response == "Top! Laten we doorgaan naar de volgende fase.":
+                with st.chat_message("assistant", avatar='🔵'):
+                    st.button("Volgende")
 
-def generate_assistant_response(response):
-    """
-    Generate a response from the assistant.
-    """
-    role_prompt = open("./assets/prompts/probleemstelling_bepalen.txt").read() + f"\nBedrijf van cursist: {company_name}" + f"\nCursus: {course}"
-    stream = client.chat.completions.create(
-        model=st.session_state.openai_model,
-        messages=[
-            {"role": "system", "content": role_prompt},
-            *({"role": m["role"], "content": m["content"]} for m in st.session_state.messages)
-        ],
-        stream=True,
-    )
-    
-    yield stream
-    # return stream
+    def main(self):
+        self.initialize_session_state()
 
+        st.title("Probleemstelling bepalen")
 
-def handle_user_input(client, company_name, course):
-    """
-    Process user input and generate a response.
-    """
-    if user_input := st.chat_input("Jouw antwoord"):
-        add_to_user_responses(user_input)
-        with st.chat_message("user", avatar='🔘'):
-            st.markdown(f"{user_input}")
+        if st.session_state.messages == []:
+            intro_text = f"Laten we de probleemstelling formuleren. Hoe zou jij jouw probleemstelling formuleren met betrekking tot {self.course} bij {self.company_name}?"
+            self.add_to_assistant_responses(intro_text)
 
-        with st.chat_message("assistant", avatar='🔵'):
-            stream = generate_assistant_response(user_input)
-            response = st.write_stream(stream)
-            add_to_assistant_responses(response)
+        self.display_chat_messages()
+        self.handle_user_input()
 
 
 if __name__ == "__main__":
-    client = connect_to_openai()
-
-    initialize_session_state()
-
-    st.title("Probleemstelling bepalen")
-    company_name = 'LearnLoop'
-    course = 'Customer Experience & Journey Management'
-
-    if st.session_state.messages == []:
-        intro_text = f"Laten we de probleemstelling formuleren. Hoe zou jij jouw probleemstelling formuleren met betrekking tot {course} bij {company_name}?"
-        add_to_assistant_responses(intro_text)
-
-    display_chat_messages()
-
-    handle_user_input(client, company_name, course)
+    probleemstelling = Probleemstelling()
+    probleemstelling.main()
